03890c7a99d57a4f108fc3ccc2afc5543763b34e
DESKTOP-4AAPR0I\tranh <tranhaockpt@gmail.com>
2017-01-21 00:22:38 +0700

    add button , scene gameOver,Victory

---
+++ Assets/Scripts/SceneControllers/VictoryScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VictoryScript : MonoBehaviour {
+
+    public void gotoVictory()
+    {
+        TKSceneManager.ChangeScene(TKSceneManager.VICTORY_SCENE);
+    }
+    public void gotoMenu()
+    {
+        TKSceneManager.ChangeScene(TKSceneManager.MENU_SCENE);
+
+    }
+}
