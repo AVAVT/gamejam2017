7e661aa3f566858b007a54d89d729cd85ee4ed53
AVAVT <itsavavt@gmail.com>
2017-01-20 23:18:15 +0700

    Scenes

---
+++ Assets/Scripts/SceneControllers/MenuScene.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuScene : MonoBehaviour {
+
+	public void StartGame(){
+		TKSceneManager.ChangeScene (TKSceneManager.GAME_PLAY_SCENE);
+	}
+}
