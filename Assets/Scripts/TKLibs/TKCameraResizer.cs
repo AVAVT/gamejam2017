10f21e908b60ab16a9777ba79a35e6f47e3326cd
AVAVT <itsavavt@gmail.com>
2017-01-20 23:03:27 +0700

    Initial commit

---
+++ Assets/Scripts/TKLibs/TKCameraResizer.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TKCameraResizer : MonoBehaviour {
+	public static readonly float REFERENCE_DEVICE_HALF_WIDTH = 540.0f;
+
+	void Awake(){
+		Camera.main.orthographicSize = REFERENCE_DEVICE_HALF_WIDTH / Camera.main.aspect;
+	}
+}
