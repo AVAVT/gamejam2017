014e4b0bc735631ad5f49f506ca8e65b9f6e0b64
AVAVT <itsavavt@gmail.com>
2017-01-20 23:14:15 +0700

    Scenes

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioManager : MonoBehaviour {
+	public static AudioManager Instance { get; private set; }
+
+	void Awake(){
+		if (Instance == null) {
+			Instance = this;
+		}
+	}
+
+	public AudioSource UIToggleSource;
+	public AudioSource UIClickSource;
+
+
+	public void PlayClickSound(){
+		UIClickSource.Play ();
+	}
+	public void PlayToggleSound(){
+		UIToggleSource.Play ();
+	}
+
+}
---
+++ Assets/Scripts/GameConfigs.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GameConfigs : MonoBehaviour
+{
+	public static GameConfigs Instance { get; private set; }
+
+	void Awake ()
+	{
+		if (Instance == null) {
+			Instance = this;
+			DontDestroyOnLoad (this);
+			Init ();
+		}
+	}
+
+	private void Init ()
+	{
+		
+    }
+		
+	#region GAME CONSTANTS
+
+
+	public static readonly string PICKUP_TEXT_COIN = "KA-CHING";
+
+
+
+	#endregion
+
+	#region GAME SETTINGS
+
+
+    #endregion
+}
+
+
