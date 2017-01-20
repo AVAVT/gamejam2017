d569be7305505960071be5226b9df372d3033b91
dev90linh <dev90.linh@gmail.com>
2017-01-21 04:44:55 +0700

    add Enemy Prefab + Script

---
+++ Assets/Scripts/ComponentControllers/GamePlayScene/Enemy.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+
+    private const float SPAWN_Y = 350;
+    private const float SPAWN_RANGE_X = 520;
+    private const float SPEED_TB = -1;
+    private const float RANGE = 0.5f;
+
+    private float speed;
+
+    public void Spawn()
+    {
+        gameObject.SetActive(true);
+        speed = Random.Range(SPEED_TB - RANGE, SPEED_TB + RANGE);
+        transform.position = new Vector2(Random.Range(-SPAWN_RANGE_X, SPAWN_RANGE_X), SPAWN_Y);
+    }
+    void Update(){
+        transform.Translate(0, speed, 0);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag.Equals("Bottom"))
+        {
+            Debug.Log("Enemy collision Bottom");
+            gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Scripts/ComponentControllers/GamePlayScene/EnemyPool.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class EnemyPool : MonoBehaviour
+{
+
+    public GameObject enemyPrefab;
+    public int poolAmount = 20;
+    List<GameObject> enemies = new List<GameObject>();
+
+    void Start()
+    {
+        for(int i =0;i < poolAmount; i++){
+            GameObject obj = Instantiate(enemyPrefab);
+            obj.SetActive(false);
+            enemies.Add(obj);
+        }
+
+        StartCoroutine(Spawn(1));
+    }
+
+    IEnumerator Spawn(float delayTime)
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(delayTime);
+            GameObject go = GetPooledObject();
+            if (go != null)
+            {
+                Enemy enemy = go.GetComponent<Enemy>();
+                go.SetActive(true);
+                enemy.Spawn();
+            }
+        }
+
+    }
+
+    public GameObject GetPooledObject()
+    {
+        for (int i = 0; i < enemies.Count; i++)
+        {
+            if (!enemies[i].activeInHierarchy)
+            {
+                return enemies[i];
+            }
+        }
+        return null;
+    }
+}
