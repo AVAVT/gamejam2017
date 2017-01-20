cb9f1840b100d4e8eb24436a445ce38844009372
AVAVT <itsavavt@gmail.com>
2017-01-21 02:52:25 +0700

    Controllers

---
+++ Assets/Scripts/ComponentControllers/GamePlayScene/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class PlayerController : MonoBehaviour
+{
+	public Vector3 acceleration;
+	public float maxSpeed;
+
+    private Rigidbody rib;
+
+    void Awake() {
+        rib = GetComponent<Rigidbody>();
+    }
+
+	void FixedUpdate ()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+
+		Vector3 direction = new Vector3 (moveHorizontal, 0, 0);
+		direction.Scale (acceleration);
+		if (direction.x / rib.velocity.x < 0 
+			|| (rib.velocity.x / maxSpeed > -1 && rib.velocity.x / maxSpeed < 1)) {
+
+			rib.AddForce (direction);
+		}
+	}
+}
