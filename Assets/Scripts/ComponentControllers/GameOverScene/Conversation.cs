25b0b45e2e4f5df54a08c0ef2a8fa31d713ced9d
DESKTOP-TVC0PFL\DUC THANG <ducthangwru97@gmail.com>
2017-01-22 05:37:12 +0700

    aa

---
+++ Assets/Scripts/ComponentControllers/GameOverScene/Conversation.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using System.IO;
+
+public class Conversation : MonoBehaviour {
+    public Text textConversation;
+    public int i = 0;
+    public int timeCount = 0;
+    public StreamReader sr = new StreamReader("Assets/Text/ending.txt");
+
+    private List<string> textarr = new List<string>();
+
+	void Start () {
+        //conversation("bac", "xyz");
+        string line;
+
+        while ((line = sr.ReadLine()) != null)
+        {
+            textarr.Add(line);
+            print(line);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timeCount++;
+        if (timeCount > 100)
+        {
+            test();
+            timeCount = 0;
+        }
+            
+	}
+
+    public void test()
+    {
+        if (i < textarr.Count)
+            textConversation.text = textarr[i++];
+    }
+}
